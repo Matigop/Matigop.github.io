2e74ecd9de9239348d5bf46e8168fdad149d3194
Matigop <165192508+Matigop@users.noreply.github.com>
2024-04-05 14:52:53 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+<<<<<<< HEAD
+
+
+
+$( document ).ready(function() {
+    var arr = ['bg_1.jpg','bg_2.jpg','bg_3.jpg'];
+    
+    var i = 0;
+    setInterval(function(){
+        if(i == arr.length - 1){
+            i = 0;
+        }else{
+            i++;
+        }
+        var img = 'url(../assets/images/'+arr[i]+')';
+        $(".full-bg").css('background-image',img); 
+     
+    }, 4000)
+
+});
+
+
+
+=======
+
+
+
+$( document ).ready(function() {
+    var arr = ['bg_1.jpg','bg_2.jpg','bg_3.jpg'];
+    
+    var i = 0;
+    setInterval(function(){
+        if(i == arr.length - 1){
+            i = 0;
+        }else{
+            i++;
+        }
+        var img = 'url(../assets/images/'+arr[i]+')';
+        $(".full-bg").css('background-image',img); 
+     
+    }, 4000)
+
+});
+
+
+
+>>>>>>> 66f8a4c3a5e692f4409ec1718a2d5427a771ed1a
